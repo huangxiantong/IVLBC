7c212b3c32153f13658408282194a48694e8476d
haungxiantong <53613471+haungxiantong@users.noreply.github.com>
2022-07-31 19:23:40 +0800

    Add files via upload

---
+++ IVLBC_80_rk/ivlbcDifftest_rk.py
@@ -0,0 +1,13 @@
+from IVLBCDiff_rk import IVLBCDiff_rk
+
+if __name__ == "__main__":
+
+	ROUND = int(input("Input the target round number: "))
+	while not (ROUND > 0):
+		print("Input1 a round number greater than 0.")
+		ROUND = int(input("Input the target round number again: "))
+	IVLBC =IVLBCDiff_rk(ROUND)
+
+	IVLBC.MakeModel()
+
+	IVLBC.SolveModel()
